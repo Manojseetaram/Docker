ad4db7413b4670e2752aec48a4101cfe5b6e7f23
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-25 15:19:47 +0530

    add the thailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,14 @@
-import { useState } from "react";
+
 import "./App.css";
-import Sidebar from "./components/Sidebar";
-import Dashboard from "./pages/Dashboard";
-
-import Images from "./pages/Images";
-import Logs from "./pages/Logs";
-import Terminal from "./pages/Terminal";
-import Containers from "./pages/container";
-
-export type Page = "dashboard" | "containers" | "images" | "logs" | "terminal";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState<Page>("dashboard");
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case "dashboard": return <Dashboard />;
-      case "containers": return <Containers />;
-      case "images": return <Images />;
-      case "logs": return <Logs />;
-      case "terminal": return <Terminal />;
-      default: return <Dashboard />;
-    }
-  };
+ 
 
   return (
-    <div className="app-shell">
-      <Sidebar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <main className="main-content">
-        {renderPage()}
-      </main>
-    </div>
+    <main className="container bg-yellow-800">
+      <h1 className="text-8xl">Welcome to Tauri + React</h1>
+
+    </main>
   );
 }
 
