80f66fff17b42f4fd83640c5f12943580b30ddf2
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-25 17:20:06 +0530

    change the structer for mac os

--- src-tauri/src/models/stats.rs
+++ src-tauri/src/models/stats.rs
@@ -1,8 +1,9 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
-pub struct SystemStats {
-    pub total_containers: usize,
-    pub running_containers: usize,
-    pub total_images: usize,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ContainerStats {
+    pub name: String,
+    pub cpu: String,
+    pub memory: String,
+    pub memory_percent: String,
 }
